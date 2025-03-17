a25346ad9a1d938cf676028124c46ce3298ddc56
manikandancms <cmanikandan23496@gmail.com>
2025-03-18 00:23:12 +0530

    javascript basic structure add

---
+++ js.basic/arith.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 5;
+
+
+let addition = num1 + num2;
+console.log("Addition:", addition);
+
+
+let subtraction = num1 - num2;
+console.log("Subtraction:", subtraction);
+
+
+let multiplication = num1 * num2;
+console.log("Multiplication:", multiplication);
+
+let division = num1 / num2;
+console.log("Division:", division);
---
+++ js.basic/func.js
@@ -0,0 +1,8 @@
+function greetUser(name){
+    return "hello, "+name
+}
+
+let nameUser = "mani"
+let myname =greetUser(nameUser)
+
+console.log(myname)
